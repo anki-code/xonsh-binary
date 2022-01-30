6a63eb9182e0dc6936552892e455dabf1e7a46fc
Andy Kipp <anki-code@users.noreply.github.com>
2022-01-30 22:50:37 +0300

    Create Building xonsh with Nuitka draft.sh

---
+++ drafts/Building xonsh with Nuitka draft.sh
@@ -0,0 +1,12 @@
+docker run -it --rm ubuntu:18.04
+
+apt update && apt install patchelf python3 python3-pip gcc make zlib1g-dev git
+pip3 install nuitka zstandard
+
+mkdir /xonsh
+cd xonsh
+git clone -n https://github.com/xonsh/xonsh && cd xonsh && git checkout 7168b26
+
+nuitka3 --static-libpython=yes --standalone --onefile --onefile-tempdir xonsh
+
+./xonsh.bin
