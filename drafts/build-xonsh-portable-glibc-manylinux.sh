a34a79a022e7dbb473c02a76c5583c9dc07d0baa
Andy Kipp <anki-code@users.noreply.github.com>
2023-07-15 13:23:09 +0600

    Delete drafts directory

--- drafts/build-xonsh-portable-glibc-manylinux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build . -f xonsh-portable-glibc-manylinux.Dockerfile -t xonsh/xonsh-portable-glibc-manylinux  #--no-cache --force-rm
-docker run --rm -v `pwd`/result:/result xonsh/xonsh-portable-glibc-manylinux
