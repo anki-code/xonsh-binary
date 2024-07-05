4e0192b08d2c960d29c2b56af778ee1aa378b01c
Andy Kipp <anki-code@users.noreply.github.com>
2024-07-06 01:34:34 +0200

    Update mamba.sh

--- draft/mamba.sh
+++ draft/mamba.sh
@@ -4,7 +4,12 @@ source ~/.bashrc
 micromamba activate base
 micromamba install -c conda-forge libpython-static==3.11.3 gcc ccache
 pip install nuitka
+
 git clone -b executables https://github.com/xonsh/xonsh
+cd xonsh
+find ./xonsh -type f -name "*.py" -print0 | xargs -0 sed -i 's/import ctypes/#import ctypes/g'
+cd ..
+
 echo "import subprocess; print(123)" > sp.py
 nuitka3 --static-libpython=yes --standalone --show-progress --show-scons --show-modules --assume-yes-for-downloads --jobs=6  sp.py
 nuitka3 --static-libpython=yes --standalone --python-flag=nosite,-O,-v --show-progress --show-scons --show-modules --assume-yes-for-downloads --jobs=6  sp.py
