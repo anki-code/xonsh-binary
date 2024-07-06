91dcf374be9f6213b9f065b3c0261236f4950ab2
Andy Kipp <anki-code@users.noreply.github.com>
2024-07-06 15:09:16 +0200

    Update mamba.sh

--- draft/mamba.sh
+++ draft/mamba.sh
@@ -3,13 +3,14 @@ apt update && apt install -y curl git vim patchelf elfutils binutils-common binu
 source ~/.bashrc
 micromamba activate base
 micromamba install -c conda-forge libpython-static==3.11.3 gcc ccache
-pip install nuitka
+pip install nuitka xonsh[full]
+pip uninstall xonsh
 
 git clone -b executables https://github.com/xonsh/xonsh
-cd xonsh
-find ./xonsh -type f -name "*.py" -print0 | xargs -0 sed -i 's/import ctypes/#import ctypes/g'
-sed -i 's/def LIBC():/def LIBC():\n    return None/g' ./xonsh/platform.py
-cd ..
+#cd xonsh
+#find ./xonsh -type f -name "*.py" -print0 | xargs -0 sed -i 's/import ctypes/#import ctypes/g'
+#sed -i 's/def LIBC():/def LIBC():\n    return None/g' ./xonsh/platform.py
+#cd ..
 
 echo "import subprocess; print(123)" > sp.py
 nuitka3 --static-libpython=yes --standalone --show-progress --show-scons --show-modules --assume-yes-for-downloads --jobs=6  sp.py
